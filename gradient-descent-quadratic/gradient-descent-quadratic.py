41fb2522b405efa3e0c8924033a4553d13168b9b
Gokul P Bharathan <67820068+Gokul2406@users.noreply.github.com>
2026-06-10 07:12:30 +0530

    Solve gradient-descent-quadratic

--- gradient-descent-quadratic/gradient-descent-quadratic.py
+++ gradient-descent-quadratic/gradient-descent-quadratic.py
@@ -3,7 +3,6 @@ def gradient_descent_quadratic(a, b, c, x0, lr, steps):
     Return final x after 'steps' iterations.
     """
     # Write code here
-    f = lambda x: a*x**2 + b*x + c
     f_dash = lambda x: 2*a*x + b
     x = x0
     for _ in range(steps):
